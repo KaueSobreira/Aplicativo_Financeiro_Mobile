1b4ff4447ab1b4069f4f58a21f82632ed2bfadb2
KaueFateb <kaue.sobreira.lucena@fateb.br>
2025-11-12 22:13:26 -0300

    feat: implementado dashboard fitcitia para usuario visualizar

--- lib/main.dart
+++ lib/main.dart
@@ -24,14 +24,205 @@ class ExpenseListPage extends StatefulWidget {
 }
 
 class _ExpenseListPageState extends State<ExpenseListPage> {
+  String? tipoFiltroSelecionado;
+  DateTime? dataFiltagem;
+  final TextEditingController dataControllerFiltragem = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Column(
+              children: [
+                Text("Selecione o Tipo de Filtro"),
+            DropdownButtonFormField<String>(
+              initialValue: tipoFiltroSelecionado,
+              decoration: InputDecoration(
+              hintText: "Tipo de Filtro",
+              border: OutlineInputBorder(),
+              ),
+                items: [
+                  DropdownMenuItem(value: "Por Mês", child: Text("Por Mês")),
+                  DropdownMenuItem(value: "Por Categoria", child: Text("Por Categoria")),
+                ],
+                  onChanged: (valor) {
+                    setState(() {
+                      tipoFiltroSelecionado = valor;
+                    });
+                  },
+                ),
+              ],
+            ),
+            tipoFiltroSelecionado == "Por Mês"
+              ? TextField(
+                  controller: dataControllerFiltragem,
+                  decoration: const InputDecoration(hintText: "Selecione a Data"),
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? dataEscolhida = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+
+                    if (dataEscolhida != null) {
+                      setState(() {
+                        dataFiltagem = dataEscolhida;
+                        dataControllerFiltragem.text =
+                            "${dataEscolhida.day}/${dataEscolhida.month}/${dataEscolhida.year}";
+                      });
+                    }
+                  },
+                )
+              : TextField(
+                  decoration: InputDecoration(hintText: "Descrição"),
+                ),
+            Card(
+              color: Colors.black,  // Margem externa
+              child: Container(         
+                constraints: BoxConstraints(
+                  minWidth: 1000,
+                ),    // Precisa de um child para mostrar conteúdo
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total Despesas em Aberto', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20
+                        ),
+                    ),
+                    Text(
+                      '10.000,00', 
+                       style: TextStyle(
+                        color: Colors.red, 
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.black,  // Margem externa
+              child: Container(
+                constraints: BoxConstraints(
+                  minWidth: 1000,
+                ),             // Precisa de um child para mostrar conteúdo
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total Despesas já pagas', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                    ),
+                    Text(
+                      '5.000,00', 
+                       style: TextStyle(
+                        color: Colors.green, 
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.black,  // Margem externa
+              child: Container(
+                constraints: BoxConstraints(
+                  minWidth: 1000,
+                ),             // Precisa de um child para mostrar conteúdo
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Text(
+                      'Categoria com mais Gastos', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                    ),
+                    Text(
+                      'Alimentação', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.black,  // Margem externa
+              child: Container(
+                constraints: BoxConstraints(
+                  minWidth: 1000,
+                ),             // Precisa de um child para mostrar conteúdo
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total Despesas do Próximo Mês', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                    ),
+                    Text(
+                      '35.000,00', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.black,  // Margem externa
+              child: Container(
+                constraints: BoxConstraints(
+                  minWidth: 1000,
+                  minHeight: 350,
+                ),             // Precisa de um child para mostrar conteúdo
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Listagem de Despesas', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                    ),
+                    Text(
+                      'Aqui vai ser a List View', 
+                       style: TextStyle(
+                        color: Colors.white, 
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 mostrarDialogCadastro();
